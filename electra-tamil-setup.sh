c364ad170fcbd1b7cfb807c1fab13cf12384361e
vanangamudi <selva.developer@gmail.com>
2021-09-03 01:52:32 +0530

    use small model

--- electra-tamil-setup.sh
+++ electra-tamil-setup.sh
@@ -21,6 +21,7 @@ git clone https://github.com/vanangamudi/electra-tamil.git
 
 cd electra-tamil
 git checkout -b chaii-finetune
+
 cd ..
 
 mkdir -p $DATA_DIR/finetuning_data/squad
@@ -31,10 +32,10 @@ echo "finetuning..."
 python3 electra-tamil/run_finetuning.py \
 	--data-dir $DATA_DIR \
 	--model-name $MODEL_NAME \
-	--hparams '{"task_names": ["squad"] }'
+	--hparams '{"task_names": ["squad"], "model_size": "small" }'
 
 
 python3 electra-tamil/run_finetuning.py \
 	--data-dir $DATA_DIR \
 	--model-name $MODEL_NAME \
-	--hparams '{"do_train": false, "do_eval": true,  "task_names": ["squad"], "init_checkpoint": "$DATA_DIR/models/$MODEL_NAME/finetuning_models/squad_model_1"}'
+	--hparams '{"do_train": false, "do_eval": true, "model_size": "small",  "task_names": ["squad"], "init_checkpoint": "$DATA_DIR/models/$MODEL_NAME/finetuning_models/squad_model_1"}'
